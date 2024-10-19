c6378f53cacb436a79f5c3ea362861c262d613fe
sanandev05 <senanalonegames@gmail.com>
2024-10-19 23:05:21 +0400

    Tasks completed

---
+++ Task-Array,Big O notation,Object/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_Array_Big_O_notation_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Verilmiş ededler siyahisindaki(int[]) en böyük ededi tapan alqoritm
+            #region Task1
+            //int[] numbers = new int[] { 185, 225, 63, 241, 365, 116, 111, 62, 64 };
+            //int largestNum=numbers[0];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > largestNum)
+            //    {
+            //        largestNum = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine($"LARGEST NUM:{largestNum}");
+            #endregion
+
+            //2. Verilmiş Products arrayindəki Product objectlərinin Id'ləri tək olanlarının Price'larının ədədi ortasını tapan algorithm yazın.
+            //(Product objectinin propertyləri: Id(yeni reqem ve ya eded), Name, Price, StockCount)
+            #region Task2
+            //var Product1 = new { ID = 1, Name = "Clock", Price = 232, StockCount = 23 };
+            //var Product2 = new { ID = 2, Name = "Display", Price = 1520, StockCount = 8 };
+            //var Product3 = new { ID = 3, Name = "RGB Mouse", Price = 60, StockCount = 22 };
+            //var Product4 = new { ID = 4, Name = "RGB Fan", Price = 150, StockCount = 2 };
+            //var Product5 = new { ID = 5, Name = "Mousepad", Price = 50, StockCount = 55 };
+
+            //var Products= new[] { Product1, Product2, Product3, Product4, Product5 };
+
+            //int avg = 0;
+            //int count=0;
+            //foreach(var product in Products)
+            //{
+            //    if (product.ID % 2 == 1)
+            //    {
+            //        avg += product.Price;
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Averages of odd id products:{avg}");
+            #endregion
+        }
+    }
+}
